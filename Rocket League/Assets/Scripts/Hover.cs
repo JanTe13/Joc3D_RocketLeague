da36f2922a22873056fef173027f762872684567
JanTeruel <jan.teruel@est.fib.upc.edu>
2017-05-09 14:43:00 +0200

    Jump and gravity

--- Rocket League/Assets/Scripts/Hover.cs
+++ Rocket League/Assets/Scripts/Hover.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Hover : MonoBehaviour {
 	
-	public float fMag = 10.0f;
-	public float impulseMag = 1500.0f;
-	public float turn = 1.0f;
+	public float fMag = 50.0f;
+	public float impulseMag = 3000.0f;
+	public float turn = 7000.0f;
+	public float forceMagnitude = 10000.0f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -27,30 +29,34 @@ public class Hover : MonoBehaviour {
 		Physics.Raycast(leftFront + 0.1f * transform.up, -transform.up, out hLeftFront);
 		Physics.Raycast(rightFront + 0.1f * transform.up, -transform.up, out hRightFront);
 		
-		Debug.DrawRay(leftRear, -transform.up, (hLeftRear.distance < 1.0f)?Color.red:Color.black);
-		Debug.DrawRay(rightRear, -transform.up,(hRightRear.distance < 1.0f)?Color.red:Color.black);
-		Debug.DrawRay(leftFront, -transform.up, (hLeftFront.distance < 1.0f)?Color.red:Color.black);
-		Debug.DrawRay(rightFront, -transform.up, (hRightFront.distance < 1.0f)?Color.red:Color.black);
+		Debug.DrawRay(leftRear, -transform.up, (leftRear.y < 1.1f)?Color.red:Color.black);
+		Debug.DrawRay(rightRear, -transform.up,(rightRear.y < 1.1f)?Color.red:Color.black);
+		Debug.DrawRay(leftFront, -transform.up, (leftFront.y < 1.1f)?Color.red:Color.black);
+		Debug.DrawRay(rightFront, -transform.up, (rightFront.y < 1.1f)?Color.red:Color.black);
 		
 		// Suspension
-		/*if(hLeftRear.distance < 1.0f)
-			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - hLeftRear.distance) * fMag * hLeftRear.normal, leftRear);
-		if(hRightRear.distance < 1.0f)
-			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - hRightRear.distance) * fMag * hRightRear.normal, rightRear);
-		if(hLeftFront.distance < 1.0f)
-			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - hLeftFront.distance) * fMag * hLeftFront.normal, leftFront);
-		if(hRightFront.distance < 1.0f)
-			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - hRightFront.distance) * fMag * hRightFront.normal, rightFront);*/
+		if(leftRear.y < 1.0f)
+			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - leftRear.y) * fMag * hLeftRear.normal, leftRear);
+		if(rightRear.y < 1.0f)
+			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - rightRear.y) * fMag * hRightRear.normal, rightRear);
+		if(leftFront.y < 1.0f)
+			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - leftFront.y) * fMag * hLeftFront.normal, leftFront);
+		if(rightFront.y < 1.0f)
+			GetComponent<Rigidbody>().AddForceAtPosition((1.0f - rightFront.y) * fMag * hRightFront.normal, rightFront);
 
 		//Impulse
-		if (hLeftRear.distance < 1.0f && hRightRear.distance < 1.0f) {
+		if (leftRear.y < 3f && rightRear.y < 3f) {
 			if (Input.GetAxis ("Vertical") == 1) {
-				impulseMag = 1500.0f;
+				impulseMag = 5000.0f;
 				GetComponent<Rigidbody> ().AddForceAtPosition (impulseMag * Input.GetAxis ("Vertical") * transform.forward, transform.position - 0.3f * transform.up);
 			} else {
 				impulseMag = 800;
 				GetComponent<Rigidbody> ().AddForceAtPosition (impulseMag * Input.GetAxis ("Vertical") * transform.forward, transform.position - 0.3f * transform.up);
 			}
+		}
+		else {
+			impulseMag = 200;
+			GetComponent<Rigidbody> ().AddForceAtPosition (impulseMag * Input.GetAxis ("Vertical") * transform.forward, transform.position - 0.3f * transform.up);
 		}
 
 		//Rotation
@@ -58,6 +64,11 @@ public class Hover : MonoBehaviour {
 
 		//Traction
 		GetComponent<Rigidbody>().AddForce(- Vector3.Dot(GetComponent<Rigidbody>().velocity, transform.right) * transform.right);
-	
+
+		//Jump
+		if (leftRear.y < 1.1f) {
+			if (Input.GetKey (KeyCode.Space))
+				gameObject.GetComponent<Rigidbody> ().AddForceAtPosition (forceMagnitude * Vector3.up, transform.position);
+		}
 	}
 }
---
+++ Rocket League/Assets/Scripts/prova.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prova : MonoBehaviour {
+
+	public float speed = 10f;
+	public float jumpForce = 8f;
+	public float gravity = 30f;
+	private Vector3 moveDir = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = gameObject.GetComponent<CharacterController> ();
+
+		if (controller.isGrounded) {
+			moveDir = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDir = transform.TransformDirection (moveDir);
+			moveDir *= speed;
+
+			if (Input.GetKey (KeyCode.Space)) {
+				moveDir.y = jumpForce;
+			}
+		}
+
+		moveDir.y -= gravity * Time.deltaTime;
+
+		controller.Move (moveDir * Time.deltaTime);
+	}
+}
