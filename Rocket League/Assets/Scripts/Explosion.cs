e5c54ab7d712c4bb55606a7414c4c710e2fef0ab
JanTeruel <jan.teruel@est.fib.upc.edu>
2017-05-15 18:24:35 +0200

    Explosió pilota + Menú (no acabat)

---
+++ Rocket League/Assets/Scripts/Explosion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	void Explode() {
+		var exp = GetComponent<ParticleSystem>();
+		exp.Play();
+		GetComponent<MeshRenderer>().enabled = false;
+		Destroy(gameObject,exp.duration);
+	}
+
+	// Grenade explodes on impact.
+	void OnCollisionEnter(Collision coll) {
+		if (coll.gameObject.tag == "Goal") 
+			Explode();
+	}
+}
