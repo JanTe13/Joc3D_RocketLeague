252ebf399d7d94551b1bb73397c3cf812e163ab8
JanTeruel <jan.teruel@est.fib.upc.edu>
2017-05-17 10:42:49 +0200

    AddExplosionForce added

--- Rocket League/Assets/Scripts/Explosion.cs
+++ Rocket League/Assets/Scripts/Explosion.cs
@@ -4,13 +4,22 @@ using System.Collections;
 public class Explosion : MonoBehaviour {
 
 	public ParticleSystem partsyst1 = null;
+	public float force = 30000.0f;
+	public float radius = 200.0f;
+	public float upwardsModifier = 0.0f;
+	public ForceMode forceMode;
 
 	void Explode() {
-		partsyst1.Play();
+		partsyst1.Play ();
+		foreach (Collider col in Physics.OverlapSphere(transform.position, radius)) {
+			Rigidbody rb = col.GetComponent<Rigidbody>();
+			if (rb != null)
+				rb.AddExplosionForce(force, transform.position, radius, upwardsModifier, forceMode);
+		}
 	}
 
 	void OnCollisionEnter(Collision coll) {
-		if (coll.gameObject.tag == "Ball") 
+		if (coll.gameObject.tag == "Ball")
 			Explode();
 	}
 }
