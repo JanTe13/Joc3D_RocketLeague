80b3b7175ed971b153d64335699c74b0b639e887
edgarripoll95 <edgar.ripoll@est.fib.upc.edu>
2017-05-24 13:07:29 +0200

    Camera Ball [Added]

--- Rocket League/Assets/Scripts/CamMove.cs
+++ Rocket League/Assets/Scripts/CamMove.cs
@@ -30,42 +30,68 @@ public class CamMove : MonoBehaviour
     public float heightDamping;
     public float rotationDamping;
 
+	private bool CameraBall;
+
+	void Start() {
+		CameraBall = false;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if (!CameraBall)
+				CameraBall = true;
+			else
+				CameraBall = false;
+		}
+	}
+
     void LateUpdate()
     {
-        // Early out if we don't have a target
-        if (!target)
-            return;
+		if (!CameraBall) {
 
-        // Calculate the current rotation angles
+			// Early out if we don't have a target
+			if (!target)
+				return;
 
-        float wantedRotationAngle = target.eulerAngles.y;
-        float wantedHeight = target.position.y + height;
-        float currentRotationAngle = transform.eulerAngles.y;
-        float currentHeight = transform.position.y;
+			// Calculate the current rotation angles
 
-        // Damp the rotation around the y-axis
-        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+			float wantedRotationAngle = target.eulerAngles.y;
+			float wantedHeight = target.position.y + height;
+			float currentRotationAngle = transform.eulerAngles.y;
+			float currentHeight = transform.position.y;
 
-        // Damp the height
-        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+			// Damp the rotation around the y-axis
+			currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
 
-        // Convert the angle into a rotation
-        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+			// Damp the height
+			currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+			// Convert the angle into a rotation
+			Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
 
 
-        // Set the position of the camera on the x-z plane to:
-        // distance meters behind the target
+			// Set the position of the camera on the x-z plane to:
+			// distance meters behind the target
 
-		var v3T = sphere.position - target.position;
-		//transform.position = sphere.position + v3T.normalized * distance;
-        transform.position = target.position;
-		transform.position -= currentRotation * Vector3.forward * distance + v3T.normalized * (distance-14);
+			var v3T = sphere.position - target.position;
+			//transform.position = sphere.position + v3T.normalized * distance;
+			transform.position = target.position;
+			transform.position -= currentRotation * Vector3.forward * distance + v3T.normalized * (distance - 14);
 
-        // Set the height of the camera
+			// Set the height of the camera
 
-        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+			transform.position = new Vector3 (transform.position.x, currentHeight, transform.position.z);
 
-        // Always look at the target
-        transform.LookAt(target);
+			// Always look at the target
+			transform.LookAt (target);
+
+		} else {
+
+
+			var v3T = sphere.position - target.position;
+			transform.position = target.position + v3T.normalized * (distance);
+			transform.LookAt (sphere);
+
+		}
     }
 }
